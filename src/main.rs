1f9cc22a7d0a6c6f0a854da4b7693c69475e60dd
juissi999 <jussi.kaatiala@gmail.com>
2022-10-11 23:16:12 +0300

    Write the main logic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::fs::remove_file;
 use std::io::Write;
 use std::env;
 
@@ -11,13 +12,22 @@ fn main() {
 
     // Open a file in write-only (ignoring errors).
     // This creates the file if it does not exist (and empty the file if it exists).
-    let mut file = File::create(temp_file).unwrap();
-
-    // Write a &str in the file (ignoring the result).
-    let mut n = 0;
-    while n < 10 {
-        writeln!(&mut file, "Hello World!").unwrap();
-        n += 1;
+    let mut file = File::create(&temp_file).unwrap();
+    
+    // Write to file until we get file i/o error
+    let mut not_finished=true;
+    while not_finished {
+        
+        if let Err(e) = file.write(b"1111111111") {
+            println!("File i/o-error. {} This is expected, and file probably filled whole hard disk and rewrite was succesfull.", e);
+            println!("Deleting file...");
+            if let Err(e) = remove_file(&temp_file) {
+                println!("File deletion error. {} Clean file by hand.", e);
+            } else {
+                println!("File deletion succesfull.");
+            }
+            not_finished=false;
+        }
     }
-
+    println!("Program finished.");
 }

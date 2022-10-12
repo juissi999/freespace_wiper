cc5b91ce8a4bae1223da4e7784472cf5bad3a473
juissi999 <jussi.kaatiala@gmail.com>
2022-10-12 20:37:21 +0300

    Add possibility to define temporary_files path

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,19 @@ use std::env;
 
 fn main() {
     let temp_directory = env::temp_dir();
-    let temp_file = temp_directory.join("file");
+    let mut temp_file = temp_directory.join("temporary_file");
+    
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() == 2  {
+        // if 1 (extra) argument is provided, it is used as temp_file path
+        // (can target other partitions as well)
+        temp_file = std::path::PathBuf::from(&args[1]);
+    }
     
     println!("freespace_wiper",);
     println!("Creating temporary file: {}", temp_file.display());
-
+    
     // Open a file in write-only (ignoring errors).
     // This creates the file if it does not exist (and empty the file if it exists).
     let mut file = File::create(&temp_file).unwrap();
@@ -18,7 +26,7 @@ fn main() {
     let mut not_finished=true;
     while not_finished {
         
-        if let Err(e) = file.write(b"1111111111") {
+        if let Err(e) = file.write(b"11111111") {
             println!("File i/o-error. {} This is expected, and file probably filled whole hard disk and rewrite was succesfull.", e);
             println!("Deleting file...");
             if let Err(e) = remove_file(&temp_file) {
